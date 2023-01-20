c9877f2a5903ccfdbb8eebf834424dd6d7e0bf9a
Ben Fitzhardinge <ben@fitzhardinge.net>
2023-01-20 11:15:11 +0800

    added product owner

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('users/', include('users.urls')),
     path("", include('projects.urls')),
 ]
---
+++ crowdfunding/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+
+from . import views
+
+urlpatterns = [
+    path('', views.CustomUserList.as_view(), name='customuser-list'),
+    path('<int:pk>/', views.CustomUserDetail.as_view(), name='customuser-detail'),
+]
