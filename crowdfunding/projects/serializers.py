d67f63e3fa42c6473618b22fba07cddfde73c993
Ben Fitzhardinge <ben@fitzhardinge.net>
2023-04-12 16:20:38 +0800

    example of a username slug field as a related name

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -11,32 +11,14 @@ class PledgeSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'supporter']
 
 
-class ProjectSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    title = serializers.CharField(max_length=200)
-    description = serializers.CharField(max_length=None)
-    goal = serializers.IntegerField()
-    image = serializers.URLField()
-    is_open = serializers.BooleanField()
-    date_created = serializers.DateTimeField()
-    owner = serializers.ReadOnlyField(source='owner_id')
+class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.SlugRelatedField(slug_field='username', read_only=True)
 
-    def create(self, validated_data):
-        return Project.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.goal = validated_data.get('goal', instance.goal)
-        instance.image = validated_data.get('image', instance.image)
-        instance.is_open = validated_data.get('is_open', instance.is_open)
-        instance.date_created = validated_data.get('date_created', instance.date_created)
-        instance.owner = validated_data.get('owner', instance.owner)
-        instance.save()
-        return instance
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description', 'goal', 'image', 'is_open', 'date_created', 'owner']
 
 
 class ProjectDetailSerializer(ProjectSerializer):
     pledges = PledgeSerializer(many=True, read_only=True)
 
-        
